4853307691804fea7fce8a6e6f3b71b0f64c8c43
Leesa Marie Price <literaryleesa@gmail.com>
2021-09-27 11:54:05 -0500

    added bootstrap settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { LoginComponent } from './login/login.component';
+
 
 const routes: Routes = [
   {path: '', component: HomepageComponent},
   { path: 'about', component: AboutComponent },
-  { path: 'login', component: LoginComponent }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
@@ -11,7 +10,7 @@ import { AboutComponent } from './about/about.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
+    
     HomepageComponent,
     NavbarComponent,
     AboutComponent
