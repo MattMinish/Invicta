054d4bd0cfb809eaa730df83213a4654fb3a6d27
Grayson Martin <grm16@students.uwf.edu>
2021-10-04 19:31:55 -0600

    adding view current trip functionality

--- src/app/cards/trip-card/trip-card.component.ts
+++ src/app/cards/trip-card/trip-card.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-trip-card',
   templateUrl: './trip-card.component.html',
   styleUrls: ['./trip-card.component.css']
 })
 export class TripCardComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+
+  public trips: any = ['Cancun', 'New York', 'Paris'];
 
 }
