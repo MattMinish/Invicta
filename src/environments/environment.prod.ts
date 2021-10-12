4a16d0e7964bc4ab96d526e7cdd5f6e37b1aae3e
MattMinish <mjm79@students.uwf.edu>
2021-10-12 12:31:19 -0500

    firebase configs and dependencies added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: "AIzaSyB-RyFBh_eddULlQI7hFEonsqGbCMVhrn4",
+    authDomain: "invicta-5ec44.firebaseapp.com",
+    projectId: "invicta-5ec44",
+    storageBucket: "invicta-5ec44.appspot.com",
+    messagingSenderId: "363429658409",
+    appId: "1:363429658409:web:95688b2d8f8804efe88138",
+    measurementId: "G-L76EGCYRWY"
+  }
 };
