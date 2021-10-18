e865b6222d952fbcf307996db557e9b7ea4af799
MattMinish <mjm79@students.uwf.edu>
2021-10-18 10:05:41 -0500

    google login works and connected to firestore

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
 
-  public username = '';
-  public password = '';
+  /** Keeps the subscription to the user object */
+  /** Keeps the subscription to the user object */
 
-  ngOnInit(): void {
+
+  private userSubscription!: Subscription;
+
+  constructor(public userService: UserService, private router: Router) { }
+
+  ngOnInit() {
+    // If a user comes back from this subscription, forward them on to the chirp page
+    this.userSubscription = this.userService.user$.subscribe(user => {
+      if (user) {
+        this.router.navigate(['login']);
+      }
+    });
+
+  }
+
+  ngOnDestroy() {
+    // Clean up the subscription if this template is destroyed
+    this.userSubscription.unsubscribe();
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../models/user';
 
-import * as firebase from 'firebase/compat';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
-
 import { Observable , of} from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 
 @Injectable({
@@ -15,34 +16,47 @@ import { switchMap } from 'rxjs/operators';
 })
 export class UserService {
 
-  user$: Observable<User>;
+  user$: Observable<User | null | undefined>;
 
   constructor(
     private afAuth: AngularFireAuth,
     private afs: AngularFirestore,
     private router: Router
   ) {
-    // Get the auth state, then fetch the Firestore user document or return null
-    this.user$ = this.afAuth.authState.pipe(
-      switchMap(user => {
-        // Logged in
-        if (user) {
-          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
-        } else {
-          // Logged out
-          return of(null);
-        }
-      })
-    );
+      // Get the auth state, then fetch the Firestore user document or return null
+      this.user$ = this.afAuth.authState.pipe(
+        switchMap(user => {
+            // Logged in
+          if (user) {
+            return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+          } else {
+            // Logged out
+            return of(null);
+          }
+        })
+      );
   }
 
+  /** Authenticates the user with Google */
   async googleSignin() {
+    console.log("here");  
     const provider = new firebase.auth.GoogleAuthProvider();
     const credential = await this.afAuth.signInWithPopup(provider);
     return this.updateUserData(credential.user);
+    
   }
 
-  private updateUserData(user) {
+  /** Signs the user out of Google */
+  async signOut() {
+    await this.afAuth.signOut();
+    this.router.navigate(['/']);
+  }
+
+  /**
+   * Saves the user data to firebase
+   * @param user The user we are updating
+   */
+  private updateUserData(user: any) {
     // Sets user data to firestore on login
     const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
 
@@ -54,4 +68,5 @@ export class UserService {
     }, { merge: true });
 
   }
+
 }
